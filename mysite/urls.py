cd3121dc9aadbc371af0be24f90316eda27327d4
Denis Zaplakhov <denis.zaplakhov@gmail.com>
2020-04-14 12:34:20 +0300

    Modified urls.py, views.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index),
     path('status', views.status),
+    path('contacts', views.contacts)
 ]
--- mysite/views.py
+++ mysite/views.py
@@ -1,7 +1,10 @@
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('Hello World')
+    return HttpResponse('<h2>Главная страница</h2><p><a href="/contacts">Страница контакты</a></p><h3>Hello World!</h3>')
 
 def status(request):
     return HttpResponse('It\'s OK')
+
+def contacts(request):
+    return HttpResponse('<h2>Страница контакты</h2><p><a href="/">Главная страница</a></p><p>Email: test@test.test</p><p>Phone: +7 (123) 456-78-99</p>')
